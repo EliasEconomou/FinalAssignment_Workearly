713ca1be35a034e290fb006e684a7ded88ba10db
EliasEconomou <eliasgybe@hotmail.com>
2023-01-12 03:14:14 +0200

    fixed csv - got most bottles per product/zip code

--- get_sales16-19.sql
+++ get_sales16-19.sql
@@ -1,9 +1,11 @@
 show databases;
 use liquorsales;
 show tables;
-select * from finance_liquor_sales;
+
+select * from finance_liquor_sales
+order by date;
 
 select *
 from finance_liquor_sales
-where date >= '2016-01-01 00:00:00' and date <= '2019-01-01 00:00:00'
+where date >= '2016-01-01 00:00:00' and date < '2020-01-01 00:00:00'
 order by date
--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-def print_test(name):
-    print(name)
+import pandas as pd
 
 
 if __name__ == '__main__':
-    print_test('Final Assignment')
+    print()
+    print('-------------------------------------------------------------------------------')
+    print()
+    salesDataDF = pd.read_csv('sales_16-19.csv')
+    pd.set_option('display.max_rows', 500)
+    print('First 5 rows:')
+    print(salesDataDF.head())
+    print()
+
+    # We'll get the number of bottles sold per item in each zip code
+    groupedZipCodes = salesDataDF.groupby(['zip_code', 'item_number'])
+    bottlesSold_perProduct_perZipCodeDF = groupedZipCodes.agg({'bottles_sold': 'sum'})
+
+    # Reset the double index
+    bottlesSold_perProduct_perZipCodeDF = bottlesSold_perProduct_perZipCodeDF.reset_index()
+
+    # Finally sort rows by bottles sold for every group and keep the first meaning the max one
+    maxesDF = bottlesSold_perProduct_perZipCodeDF.sort_values(['zip_code', 'bottles_sold'], ascending=False)\
+        .groupby(['zip_code']).first()\
+        .reset_index()
+
+    print('Product with most bottles sold per zip code:')
+    print(maxesDF)
+    print()
+    print('-------------------------------------------------------------------------------')
+    print()
+
+
+
+
+
+
+
 
